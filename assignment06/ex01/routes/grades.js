a223f0d1d396afa557965dd21ede0facdf2e51f2
Tulsi <neeman.drn@gmail.com>
2018-01-21 19:53:06 -0600

    added

---
+++ assignment06/ex01/routes/grades.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const GradeMgr = require('../entity/grades');
+const { check, validationResult } = require('express-validator/check');
+const { matchedData, sanitize } = require('express-validator/filter');
+
+let gradesMgr = new GradeMgr();
+let router = express.Router();
+
+let checkOp = [
+	check('id').exists(),
+	check('name').exists(),
+	check('course').exists(),
+	check('grade').exists(),
+  	check('id', 'Id must be atleast one character').isLength({ min: 1 }),
+  	check('name', 'name must be atleast one character').isLength({ min: 1 }),
+  	check('course', 'course must be atleast one character').isLength({ min: 1 }),
+  	check('grade', 'grade must be atleast one character').isLength({ min: 1 }),
+  	sanitize('id').toInt()
+];
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  	res.status(200).json(gradesMgr.getGrades());
+});
+
+router.post('/', checkOp, function(req, res, next) {
+
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+		    return res.status(422).json({ errors: errors.mapped() });
+		}else {
+			const gd = matchedData(req);
+			let q = gradesMgr.getGradesById(gd.id);
+
+  			if(q != null) {
+  				return res.status(422).json({ errors: "id is already present"});
+  			}
+
+	  		gradesMgr.add(gd);
+	  		res.status(200).json({"message": "done"});
+	  	}
+});
+
+router.put('/', checkOp, function(req, res, next) {
+  	const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+		    return res.status(422).json({ errors: errors.mapped() });
+		}else {
+			const gd = matchedData(req);
+			let q = gradesMgr.getGradesById(gd.id);
+
+  			if(q == null) {
+  				return res.status(422).json({ errors: "id is not present to modify"});
+  			}
+  			
+	  		gradesMgr.modify(gd);
+	  		res.status(200).json({"message": "done"});
+	  	}
+});
+
+router.delete('/:id', function(req, res, next) {
+	let id = req.params.id;
+	if(id == null) {
+		return res.status(422).json({ errors: "no id found"});
+	}
+
+	gradesMgr.remove(id);
+  	res.status(200).json({"message": "done"});
+});
+
+module.exports = router;
